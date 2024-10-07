9a598b00e6f5d0353367ff117da3f47091fda3ae
DataGeek404 <techjayprivate@gmail.com>
2024-10-07 22:56:56 +0300

    Add jay.pdf to Reports folder

--- daily_email_report.py
+++ daily_email_report.py
@@ -81,7 +81,7 @@ def send_daily_report():
     send_email_with_attachment(subject, body, to_email, cc_email, file_path)
 
 # Schedule the email to be sent every day at a specific time (e.g., 12:15 PM)
-schedule.every().day.at("22:55").do(send_daily_report)
+schedule.every().day.at("22:58").do(send_daily_report)
 
 # Run the scheduler in an infinite loop
 if __name__ == "__main__":
